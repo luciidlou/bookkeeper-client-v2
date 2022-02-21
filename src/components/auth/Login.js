c9813292a6d0bc73f79083912186946f62cbc131
luciidlou <lewisale96@gmail.com>
2022-02-21 09:33:58 -0600

    framed the Login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,22 @@
 import "./Login.css"
+import logo from "../../images/BookkeeperLogo2.png"
+
 
 export const Login = () => {
     return (
-        <div className="login-container">
-            <h1 className="is-size-2">Welcome to Bookkeeper!</h1>
+        <div className="login">
+            <figure className="image is-64x64">
+                <img src={logo} alt="Bookkeeper logo" />
+            </figure>
+            <h2 className="h2 is-size-2 has-text-centered">Bookkeeper</h2>
+            <div className="subtitle is-size-6 has-text-centered is-italic	">A user-driven, open-source book repository</div>
+            <h4 className="h2 is-size-4 has-text-centered">Login</h4>
+            <fieldset>
+                <label htmlFor="username">Username</label>
+                <input className="input" type="text" name="username" />
+                <label htmlFor="password">Password</label>
+                <input className="input" type="text" name="password" />
+            </fieldset>
         </div>
     )
 }
